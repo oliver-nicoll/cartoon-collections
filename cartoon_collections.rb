8665ea755565b59877aafc77ac5ca540db40d96c
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-04 20:22:58 -0500

    commit tree 46199f1e3109960fafe16473acd77aeb5b3cede4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+  puts "#{index + 1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+  array.map! {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  answer = false
+  calls.each do |calls|
+    if calls.length > 4
+      answer = true
+    end
+  end
+  answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  # array.detect do |cheese|
+  #   cheese_types.include?(cheese)
+    array.find {|cheese| cheese_types.include?(cheese)}
+  end
